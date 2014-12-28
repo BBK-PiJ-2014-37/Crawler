631f2fc4ed3963a723ed5960345ebc79cfbc86a3
Joel Armando <joelarma@gmail.com>
2014-12-28 22:24:46 +0000

    Added javadoc for getNextTag, and outline for missing code

--- src/org/joel/crawler/HTMLRead.java
+++ src/org/joel/crawler/HTMLRead.java
@@ -1,9 +1,6 @@
 package org.joel.crawler;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.Reader;
 import java.util.ArrayList;
 import java.util.List;
@@ -26,11 +23,29 @@ public abstract class HTMLRead {
 			if (tag.equals("")) {
 				return urls;
 			}
-			// process tag
+			// If tag is not either "a" or "base", continue
+			// get contents of "href" attribute
+			// if there was no href attribute, continue
+			// if tag is base:
+			//     if there was no previous base tag, change the base
+			// else 
+			//     build the URL from base and href
+			//     add URL to list
 		}
 	}
 		
-	
+	/**
+	 * Get the contents of the next HTML tag.
+	 * 
+	 * Finds the next tag in the input stream, and returns its contents in a
+	 * String. The string contains all the text between the "<" and ">"
+	 * characters. Whitespace in the string are converted into spaces. A series
+	 * of consecutive whitespace characters are converted to as single one.
+	 * 
+	 * @param source the input stream
+	 * @return the contents of the next tag
+	 * @throws IOException
+	 */
 	private static String getNextTag(Reader source) throws IOException {
 		String tag = "";
 		int c = 0;
