e3d2a54ad383e4a3ccb936ab44e30def4d93e139
Joel Armando <joelarma@gmail.com>
2015-02-11 21:31:25 +0000

    Implement HTTPURLFetcher

--- src/org/joel/crawler/HTTPURLFetcher.java
+++ src/org/joel/crawler/HTTPURLFetcher.java
@@ -1,13 +1,26 @@
 package org.joel.crawler;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
 import java.util.List;
 
 public class HTTPURLFetcher implements URLFetcher {
-
+	
+	/**
+	 * Fetches the content of a URL from the network
+	 *@param String url
+	 *				the url to fetch
+	 *@return the list of links read from the HTML
+	 */
 	@Override
 	public List<String> fetch(String url) {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			InputStream in = new URL(url).openStream();
+			return HTMLRead.getURLs(new InputStreamReader(in), url);
+		} catch (IOException e) {
+			return null;
+		}
 	}
-
 }
