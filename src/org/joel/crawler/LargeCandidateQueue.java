8388c0df4a56a0edacf18b05bfc2f2c4fbc5abc8
Joel Armando <joelarma@gmail.com>
2015-02-12 09:21:49 +0000

    Add implementation for LargeCandidateQueue

--- src/org/joel/crawler/LargeCandidateQueue.java
+++ src/org/joel/crawler/LargeCandidateQueue.java
@@ -1,20 +1,37 @@
 package org.joel.crawler;
 
+import java.io.IOException;
+import java.io.Writer;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Scanner;
+
 public class LargeCandidateQueue implements CandidateQueue {
+	private Scanner in;
+	private Writer out;
 
+	public LargeCandidateQueue(Path pathname) throws IOException {
+		out = Files.newBufferedWriter(pathname);
+		in = new Scanner(Files.newBufferedReader(pathname));
+	}
+	
 	public void add(Candidate c) {
-		// TODO Auto-generated method stub
-
+		try {
+			out.write("" + c.level + " " + c.url + "\n");
+			out.flush();
+		} catch (IOException e) {
+			throw new IllegalStateException();
+		}
 	}
 
 	public boolean isEmpty() {
-		// TODO Auto-generated method stub
-		return false;
+		return !in.hasNextLine() || !in.hasNextInt();
 	}
 
 	public Candidate poll() {
-		// TODO Auto-generated method stub
-		return null;
+		int level = in.nextInt();
+		String url = in.next();
+		return new Candidate(level, url);
 	}
 
 }
